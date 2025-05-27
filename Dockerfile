28a7cad07082ff49be097e3bf82b8fa4c6484b7f
AokiMasataka <4okimasataka@gmail.com>
2025-05-27 23:49:47 +0900

    feat: runnng laravel on docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
 #$ sudo apt install -y php-mysql
-FROM php:7.2-apache
+# FROM php:7.2-apache
+FROM php:8.2-apache
 
 RUN docker-php-ext-install pdo_mysql
+
+RUN apt update -y
+RUN apt install curl git vim unzip -y
+RUN curl -sS https://getcomposer.org/installer -o composer-setup.php
+RUN php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
 
 #RUN apt install -y php-mysql
---
+++ html/sample/app/Http/Controllers/ArticleUserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ArticleUser;
+use Illuminate\Http\Request;
+
+class ArticleUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ArticleUser $articleUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ArticleUser $articleUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ArticleUser $articleUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ArticleUser $articleUser)
+    {
+        //
+    }
+}
